c7dd4bbed22127e4480087e634290f45d3d6ec31
Thomas Riffard <ferdodo@hotmail.fr>
2020-04-05 03:06:05 +0200

    First test is passing.

--- test/lib/TestRunner.ts
+++ test/lib/TestRunner.ts
@@ -9,22 +9,34 @@ export default class TestRunner {
 		this.appStarted = this.spectronApp.start();
 	}
 
-	get client() {
-		return this.spectronApp.client;
-	}
-
 	run(executor: (testRunner: TestRunner) => void) {
-		return asyncRun(this, executor);
+		return asyncRun(this, executor).catch(dumpElectronLogs(this));
 	}
 
 	close() {
 		return this.spectronApp.stop();
+	}
+
+	setInput(selector: string, value: string) {
+		return asyncSetInput(this, selector, value);
 	}
 }
 
 async function asyncRun(testRunner: TestRunner, executor: (testRunner: TestRunner) => void) {
 	await testRunner.appStarted;
 	await executor(testRunner);
+}
+
+async function asyncSetInput(testRunner: TestRunner, selector: string, value: string) {
+	const client = testRunner.spectronApp.client;
+	await client.clearElement(selector);
+	await client.setValue(selector, value);
+	const verification = await client.getValue(selector);
+
+	if (verification != value) {
+		const hint = JSON.stringify({ selector, value, verification }, null, 4);
+		throw new Error(`Failed to set an input ! ${hint}`);
+	}
 }
 
 function getBinariesPath() {
@@ -42,4 +54,14 @@ function createSpectronApp() {
 	const path = getBinariesPath();
 	const args = ["--no-sandbox", "--headless", "--disable-gpu"];
 	return new Application({ path, args });
+}
+
+function dumpElectronLogs(testRunner: TestRunner) {
+	return async function (error: Error) {
+		const client = testRunner.spectronApp.client;
+		const logs = await client.getMainProcessLogs();
+		console.log("Log dump:");
+		for (var i = 0; i < logs.length; ++i) console.log(`  - ${logs[i]}`);
+		throw error;
+	};
 }
--- test/tests/1-create-config.ts
+++ test/tests/1-create-config.ts
@@ -4,30 +4,26 @@ const { random } = require("lodash");
 
 export default async function test(testRunner: TestRunner) {
 	const scriptFilePath = "/csgo-dof-screenshot/dof.cfg";
-	const minPos = -1000000;
-	const maxPos = 1000000;
-
-	await Promise.all([
-		testRunner.client.setValue("#inputCameraX", random(minPos, maxPos)),
-		testRunner.client.setValue("#inputCameraY", random(minPos, maxPos)),
-		testRunner.client.setValue("#inputCameraZ", random(minPos, maxPos)),
-		testRunner.client.setValue("#inputTargetX", random(minPos, maxPos)),
-		testRunner.client.setValue("#inputTargetY", random(minPos, maxPos)),
-		testRunner.client.setValue("#inputTargetZ", random(minPos, maxPos)),
-		testRunner.client.setValue("#inputDofStrength", random(0.000001, 10000)),
-		testRunner.client.setValue("#inputKeyBind", "o"),
-		testRunner.client.setValue("#inputScriptLocation", scriptFilePath),
-	]);
-
-	await testRunner.client.click("#buttonSaveScript");
-	await testRunner.client.getValue("#displaySaveScriptDone");
+	const minPos = -100000;
+	const maxPos = 100000;
+	await testRunner.setInput("#inputCameraX", random(minPos, maxPos));
+	await testRunner.setInput("#inputCameraY", random(minPos, maxPos));
+	await testRunner.setInput("#inputCameraZ", random(minPos, maxPos));
+	await testRunner.setInput("#inputTargetX", random(minPos, maxPos));
+	await testRunner.setInput("#inputTargetY", random(minPos, maxPos));
+	await testRunner.setInput("#inputTargetZ", random(minPos, maxPos));
+	await testRunner.setInput("#inputDofStrength", random(-3000, 3000));
+	await testRunner.setInput("#inputKeyBind", "o");
+	await testRunner.setInput("#inputScriptLocation", scriptFilePath);
+	await testRunner.spectronApp.client.click("#buttonSaveScript");
+	await testRunner.spectronApp.client.getValue("#displaySaveScriptDone");
 	const scriptBuffer = readFileSync(scriptFilePath);
 	const script = scriptBuffer.toString();
 
 	if (script.length < 50000) {
 		throw new Error("Script is too short !");
 	}
-	
+
 	const sample = script.substring(25000, 27000);
 	const re = new RegExp(/^([a-zA-Z0-9_"\.\-;]|\s)+$/);
 
