02a25a05a1852fc0a9b7b235d2838bb1f321661a
Thomas Riffard <ferdodo@hotmail.fr>
2020-04-08 09:14:47 +0200

    Prettier file.

--- app/main.js
+++ app/main.js
@@ -1,15 +1,14 @@
-const {app, BrowserWindow, ipcMain, dialog} = require('electron');
-const fs = require('fs');
-const util = require('util');
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const fs = require("fs");
+const util = require("util");
 const writeFile = util.promisify(fs.writeFile);
 const { homedir } = require("os");
 var ScreenshotsMerger = require("./lib/ScreenshotsMerger.js");
 var WeightedImage = require("./lib/WeightedImage.js");
 
-
 main();
 
-async function main(){
+async function main() {
 	await appReady();
 	var win = await createWindow();
 	ipcMain.handle('saveScript', saveScript(win));
@@ -23,44 +22,46 @@ async function getDefaultScriptPath(){
 }
 
 
-function saveScript(win){
-	return async function(event, payload){
-		await writeFile(payload.scriptLocation, payload.script); 
+function saveScript(win) {
+	return async function (event, payload) {
+		await writeFile(payload.scriptLocation, payload.script);
 	};
 }
 
-function mergeScreenshots(win){
-	return async function(event, payload){
-		var {filePaths, canceled} = await dialog.showOpenDialog(win, {"properties":["openFile","multiSelections"]});
+function mergeScreenshots(win) {
+	return async function (event, payload) {
+		var { filePaths, canceled } = await dialog.showOpenDialog(win, {
+			properties: ["openFile", "multiSelections"],
+		});
 		if (canceled) return;
 		var screenshotsMerger = new ScreenshotsMerger(win);
-		filePaths.map(path=>new WeightedImage(path)).forEach(screenshotsMerger.add, screenshotsMerger);
+		filePaths.map((path) => new WeightedImage(path)).forEach(screenshotsMerger.add, screenshotsMerger);
 		var merged = await screenshotsMerger.mergeAll();
 		var data = await util.promisify(fs.readFile)(merged.path);
-		await saveAs(win)(null, {data, "options" : {"defaultPath" : "csgo-dof-screenshot.png"}});
-	}
+		await saveAs(win)(null, { data, options: { defaultPath: "csgo-dof-screenshot.png" } });
+	};
 }
 
-async function createWindow(){
+async function createWindow() {
 	var win = new BrowserWindow({
-		"width": 490,
-		"height": 630,
-		"webPreferences": {
-			"nodeIntegration": true
-		}
+		width: 490,
+		height: 630,
+		webPreferences: {
+			nodeIntegration: true,
+		},
 	});
 
 	win.setMenuBarVisibility(false);
 	win.setResizable(false);
-	await win.loadFile('gui/dist/index.html');
+	await win.loadFile("gui/dist/index.html");
 	return win;
 }
 
-function appReady(){
-	return new Promise(function(resolve, reject){ 
+function appReady() {
+	return new Promise(function (resolve, reject) {
 		var timeout = setTimeout(reject, 5000, "Timeout !");
-		
-		app.on('ready', function(){
+
+		app.on("ready", function () {
 			clearTimeout(timeout);
 			resolve();
 		});
@@ -68,6 +69,6 @@ function appReady(){
 }
 
 // https://gist.github.com/6174/6062387
-function randomStr(){
+function randomStr() {
 	return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
