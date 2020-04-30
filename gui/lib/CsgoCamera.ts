3c593c8990d22231b552c0bbe7bd1f4048f9f277
Thomas Riffard <ferdodo@hotmail.fr>
2020-04-30 12:17:12 +0200

    Fix bad axis ordering when exorting a 3D vector to command.

--- gui/lib/CsgoCamera.ts
+++ gui/lib/CsgoCamera.ts
@@ -25,8 +25,8 @@ export default class CsgoCamera {
 		return [
 			"spec_goto",
 			`${this.position.x.toFixed(4)}`,
-			`${this.position.z.toFixed(4)}`,
 			`${this.position.y.toFixed(4)}`,
+			`${this.position.z.toFixed(4)}`,
 			`${this.yaw.toFixed(4)}`,
 			`${this.pitch.toFixed(4)}`,
 		].join(" ");
@@ -34,20 +34,20 @@ export default class CsgoCamera {
 
 	private calculatePitch(target: Vector3D) {
 		const delta = target.delta(this.position);
-		const result = Math.atan2(Math.abs(delta.z), delta.x);
+		const result = Math.atan2(Math.abs(delta.y), delta.x);
 		const resultRadian = result * (180 / Math.PI);
-		const hypothenuse = Math.sqrt(Math.pow(delta.x, 2) + Math.pow(delta.z, 2));
-		const sin = delta.z / hypothenuse;
+		const hypothenuse = Math.sqrt(Math.pow(delta.x, 2) + Math.pow(delta.y, 2));
+		const sin = delta.y / hypothenuse;
 		if (sin < 0) return -resultRadian;
 		return resultRadian;
 	}
 
 	private calculateYaw(target: Vector3D) {
 		const delta = target.delta(this.position);
-		const horizontalDistance = Math.sqrt(Math.pow(delta.x, 2) + Math.pow(delta.z, 2));
-		const result = -Math.atan2(Math.abs(delta.y), horizontalDistance);
+		const horizontalDistance = Math.sqrt(Math.pow(delta.x, 2) + Math.pow(delta.y, 2));
+		const result = -Math.atan2(Math.abs(delta.z), horizontalDistance);
 		const resultRadian = result * (180 / Math.PI);
-		if (delta.y < 0) return -resultRadian;
+		if (delta.z < 0) return -resultRadian;
 		return resultRadian;
 	}
 }
