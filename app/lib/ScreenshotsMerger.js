14c2c915cfa34bb7a79c37dcbf7d3f6a09fd4247
Thomas Riffard <ferdodo@hotmail.fr>
2020-05-01 09:10:24 +0200

    Implement multiprocessing to make image merging way faster.

--- app/lib/ScreenshotsMerger.js
+++ app/lib/ScreenshotsMerger.js
@@ -1,6 +1,7 @@
 var temp = require("temp").track();
 var util = require('util');
 var MergeProgressSender = require("./MergeProgressSender.js");
+const os = require('os');
 
 class ScreenshotsMerger{
 
@@ -25,26 +26,37 @@ class ScreenshotsMerger{
 
 
 async function _mergeAll(screenshotsMerger){
-	var mergeProgressSender = new MergeProgressSender(screenshotsMerger.images.length, screenshotsMerger.win);
-
-	while (screenshotsMerger.images.length > 1){
-		var {a,b} = screenshotsMerger.images.map(function(image){
-			var a = image;
-			var b = image.findClosest(screenshotsMerger);
-			var distance = Math.abs(a.weight - b.weight);
-			return {a, b, distance};
-		}).sort((a,b)=>b.distance-a.distance).pop();
-
-		var tempDirPath = await screenshotsMerger.waitTempDirPath;
-		var merged = await a.merge(b, tempDirPath);
-		screenshotsMerger.add(merged);
-		screenshotsMerger.remove(a);
-		screenshotsMerger.remove(b);
-		mergeProgressSender.sendProgress();
-	}
-
+	const mergeProgressSender = new MergeProgressSender(screenshotsMerger.images.length, screenshotsMerger.win);
+	const cpuCores = os.cpus().length;
+	const startMergeInstance = getInstanceStarter(screenshotsMerger, mergeProgressSender);
+	const mergeAllInstances = Array.from(Array(cpuCores)).map(startMergeInstance);
+	await Promise.all(mergeAllInstances)
+	await startMergeInstance();
 	mergeProgressSender.sendFinish();
 	return screenshotsMerger.images.pop();
+}
+
+
+function getInstanceStarter(screenshotsMerger, mergeProgressSender){
+	return async function startMergeInstance(){
+		var mergeProgressSender = new MergeProgressSender(screenshotsMerger.images.length, screenshotsMerger.win);
+
+		while (screenshotsMerger.images.length > 1){
+			var {a,b} = screenshotsMerger.images.map(function (image){
+				var a = image;
+				var b = image.findClosest(screenshotsMerger);
+				var distance = Math.abs(a.weight - b.weight);
+				return {a, b, distance};
+			}).sort((a,b)=>b.distance-a.distance).pop();
+
+			screenshotsMerger.remove(a);
+			screenshotsMerger.remove(b);
+			var tempDirPath = await screenshotsMerger.waitTempDirPath;
+			var merged = await a.merge(b, tempDirPath);
+			screenshotsMerger.add(merged);
+			mergeProgressSender.sendProgress();
+		}
+	}
 }
 
 
