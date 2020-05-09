540b2a4bf8c765e8db17c0a06c16967714d28bb0
Thomas Riffard <ferdodo@hotmail.fr>
2020-05-09 08:02:19 +0200

    Prettier file.

--- electron-renderer/lib/Vector3D.ts
+++ electron-renderer/lib/Vector3D.ts
@@ -1,9 +1,9 @@
-export default class Vector3D{
+export default class Vector3D {
 	x: number;
 	y: number;
 	z: number;
 
-	constructor(x:number, y:number, z:number){
+	constructor(x: number, y: number, z: number) {
 		this.x = Number(x);
 		this.y = Number(y);
 		this.z = Number(z);
@@ -12,17 +12,17 @@ export default class Vector3D{
 		if (!Number.isFinite(this.z)) throw new Error("Z is not a finite number!");
 	}
 
-	randomize(spread:number){
-		const x = this.x + spread * (Math.random()-0.5);
-		const y = this.y + spread * (Math.random()-0.5);
-		const z = this.z + spread * (Math.random()-0.5);
-		return new Vector3D(x,y,z);
+	randomize(spread: number) {
+		const x = this.x + spread * (Math.random() - 0.5);
+		const y = this.y + spread * (Math.random() - 0.5);
+		const z = this.z + spread * (Math.random() - 0.5);
+		return new Vector3D(x, y, z);
 	}
 
-	delta(vector3d:Vector3D){
+	delta(vector3d: Vector3D) {
 		const x = this.x - vector3d.x;
 		const y = this.y - vector3d.y;
 		const z = this.z - vector3d.z;
-		return new Vector3D(x,y,z);
+		return new Vector3D(x, y, z);
 	}
 }
