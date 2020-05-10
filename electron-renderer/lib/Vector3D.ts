6542214f88f87aa8d040a7128ba4010e698551cd
Thomas Riffard <ferdodo@hotmail.fr>
2020-05-10 14:24:54 +0200

    Add a library to generates 3D shapes.

--- electron-renderer/lib/Vector3D.ts
+++ electron-renderer/lib/Vector3D.ts
@@ -12,17 +12,27 @@ export default class Vector3D {
 		if (!Number.isFinite(this.z)) throw new Error("Z is not a finite number!");
 	}
 
-	randomize(spread: number) {
+	randomize(spread: number): Vector3D {
 		const x = this.x + spread * (Math.random() - 0.5);
 		const y = this.y + spread * (Math.random() - 0.5);
 		const z = this.z + spread * (Math.random() - 0.5);
 		return new Vector3D(x, y, z);
 	}
 
-	delta(vector3d: Vector3D) {
+	delta(vector3d: Vector3D): Vector3D {
 		const x = this.x - vector3d.x;
 		const y = this.y - vector3d.y;
 		const z = this.z - vector3d.z;
 		return new Vector3D(x, y, z);
+	}
+
+	distanceTo(vector3d: Vector3D): number{
+		const delta = this.delta(vector3d);
+		const hypothenuse2D = Math.hypot(delta.x, delta.y);
+		return Math.hypot(hypothenuse2D, delta.z);
+	}
+
+	isIntoSphere(spherePosition: Vector3D, sphereRadius: number){
+		return this.distanceTo(spherePosition) < sphereRadius;
 	}
 }
---
+++ electron-renderer/lib/scatterPlot3DShapesGenerators.ts
@@ -0,0 +1,60 @@
+import Vector3D from "./Vector3D.ts";
+const PROGRESSIVE_MIN_SIZE = 0.2;
+const PROGRESSIVE_FACTOR = 1.02;
+
+function randomLinear(a: number, b: number): number {
+	return a * (Math.random() - 0.5) + b;
+}
+
+export function* createCubeGenerator(position: Vector3D, size: number): Generator<Vector3D, Vector3D, Vector3D> {
+	const generateX = () => randomLinear(size, position.x);
+	const generateY = () => randomLinear(size, position.y);
+	const generateZ = () => randomLinear(size, position.z);
+	while (true) yield new Vector3D(generateX(), generateY(), generateZ());
+}
+
+export function* createSphereGenerator(position: Vector3D, diameter: number): Generator<Vector3D, Vector3D, Vector3D> {
+	const cubeGenerator = createCubeGenerator(position, diameter);
+	const sphereRadius = diameter / 2;
+
+	while (true) {
+		var vector = cubeGenerator.next().value;
+		if (!vector.isIntoSphere(position, sphereRadius)) continue;
+		yield vector;
+	}
+}
+
+export function* createProgressiveSphereGenerator(position: Vector3D): Generator<Vector3D, Vector3D, Vector3D> {
+	let diameter = PROGRESSIVE_MIN_SIZE;
+
+	while (true) {
+		var sphereGenerator = createSphereGenerator(position, diameter);
+		yield sphereGenerator.next().value;
+		diameter *= PROGRESSIVE_FACTOR;
+	}
+}
+
+export function* createDiscGenerator(position: Vector3D, target: Vector3D, diameter: number): Generator<Vector3D, Vector3D, Vector3D> {
+	const sphereGenerator = createSphereGenerator(position, diameter);
+	const distanceToTarget = position.distanceTo(target);
+	const distanceToTargetMax = distanceToTarget * 1.01;
+	const distanceToTargetMin = distanceToTarget * 0.99;
+
+	while (true) {
+		var vector = sphereGenerator.next().value;
+		var vectorDistanceToTarget = vector.distanceTo(target);
+		if (vectorDistanceToTarget > distanceToTargetMax) continue;
+		if (vectorDistanceToTarget < distanceToTargetMin) continue;
+		yield vector;
+	}
+}
+
+export function* createProgressiveDiscGenerator(position: Vector3D, target: Vector3D): Generator<Vector3D, Vector3D, Vector3D> {
+	let diameter = PROGRESSIVE_MIN_SIZE;
+
+	while (true) {
+		var discGenerator = createDiscGenerator(position, target, diameter);
+		yield discGenerator.next().value;
+		diameter *= PROGRESSIVE_FACTOR;
+	}
+}
